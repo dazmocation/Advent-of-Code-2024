9917a7bbb99d4f45b2c5b5147cf076a6229de70b
dazmocation <dazmocation@gmail.com>
2024-12-02 19:20:59 -0500

    primary function structure created

--- day_2.py
+++ day_2.py
@@ -1,4 +1,19 @@
 def main():
+    file = open("day_2_input.txt")
+    print(part_1())
+    file.close()
+    return
+
+
+def part_1():
+    pass
+
+
+def check_direction():
+    pass
+
+
+def check_difference():
     pass
 
 
