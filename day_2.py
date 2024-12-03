673debd89f8b4de3e6fa03ec0d65edfc74d7f0cd
dazmocation <dazmocation@gmail.com>
2024-12-02 21:53:00 -0500

    revert to basic formula after re-reading prompt

--- day_2.py
+++ day_2.py
@@ -1,15 +1,24 @@
 def main():
     file = open("day_2_input.txt")
-    print(part_1(file))
+    input_list = input_to_list(file)
+    print(part_1(input_list))
+    print(part_2(input_list))
     file.close()
     return
 
 
-def part_1(file):
-    input_list = input_to_list(file)
+def part_1(input_list):
     safe = 0
     for line in input_list:
         if check_diff_and_dir(line):
+            safe += 1
+    return safe
+
+
+def part_2(input_list):
+    safe = 0
+    for line in input_list:
+        if check_diff_and_dir_2(line):
             safe += 1
     return safe
 
@@ -25,6 +34,25 @@ def check_diff_and_dir(line):
         if previous == 0 or previous < 0 and difference < 0 or previous > 0 and difference > 0:
             previous = difference
         else:
+            return False
+    # if both valid
+    return True
+
+
+def check_diff_and_dir_2(line):
+    previous = 0
+    fail = 0
+    for i in range(len(line) - 1):
+        difference = line[i] - line[i + 1]
+        # check if the difference is not in the safe range
+        if not 1 <= abs(difference) <= 3:
+            fail += 1
+        # check that the sign of the current difference is the same as the previous difference
+        if previous == 0 or previous < 0 and difference < 0 or previous > 0 and difference > 0:
+            previous = difference
+        else:
+            fail += 1
+        if fail > 1:
             return False
     # if both valid
     return True
