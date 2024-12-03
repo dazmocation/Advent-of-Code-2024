b4a3c17e2a11e12a2dc75b404d67fe3ec43bb23e
dazmocation <dazmocation@gmail.com>
2024-12-02 20:27:27 -0500

    added abs() to range check in check_diff

--- day_2.py
+++ day_2.py
@@ -1,5 +1,5 @@
 def main():
-    file = open("day_2_input.txt")
+    file = open("day_2_input_test.txt")
     print(part_1(file))
     file.close()
     return
@@ -19,21 +19,24 @@ def check_diff_and_dir(line):
     for i in range(len(line) - 1):
         difference = line[i] - line[i + 1]
         # check if the difference is not in the safe range
-        if not 1 <= difference <= 3:
+        if not 1 <= abs(difference) <= 3:
+            print("Unsafe 1")
             return False
         # check that the sign of the current difference is the same as the previous difference
         if previous == 0 or previous < 0 and difference < 0 or previous > 0 and difference > 0:
             previous = difference
         else:
+            print("Unsafe 2")
             return False
     # if both valid
+    print("Safe")
     return True
 
 
 def input_to_list(file):
     input_list = []
     for line in file:
-        input_list.append(line.split())
+        input_list.append(list(map(int, line.split())))
     return input_list
 
 if __name__ == "__main__":
