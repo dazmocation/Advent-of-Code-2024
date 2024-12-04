ddbc1139428f7dc127322b4971302b6ceb7fa436
dazmocation <dazmocation@gmail.com>
2024-12-03 21:34:18 -0500

    updated part_1 with a working solution

--- day_3.py
+++ day_3.py
@@ -1,8 +1,18 @@
 import re
 
 def main():
-    pass
+    file = open("day_3_input.txt")
+    file_as_string = str(file.read())
+    print(part_1(file_as_string))
+    file.close()
 
+
+def part_1(file):
+    matches = re.findall(r"mul\((\d+),(\d+)\)", file)
+    count = 0
+    for item in matches:
+        count += int(item[0]) * int(item[1])
+    return count
 
 if __name__ == "__main__":
     main()
