1adb0f7ab6f6fb286ad98fe206813aa5ca2e4657
dazmocation <dazmocation@gmail.com>
2024-12-03 21:56:19 -0500

    updated with complete solution for part_2

--- day_3.py
+++ day_3.py
@@ -4,15 +4,28 @@ def main():
     file = open("day_3_input.txt")
     file_as_string = str(file.read())
     print(part_1(file_as_string))
+    print(part_2(file_as_string))
     file.close()
 
 
 def part_1(file):
     matches = re.findall(r"mul\((\d+),(\d+)\)", file)
-    count = 0
+    sum = 0
     for item in matches:
-        count += int(item[0]) * int(item[1])
-    return count
+        sum += int(item[0]) * int(item[1])
+    return sum
+
+
+def part_2(file):
+    file = file.split("do()")
+    sum = 0
+    for i in file:
+        i = i.split("don't()")[0]
+        i = re.findall(r"mul\((\d+),(\d+)\)", i)
+        for item in i:
+            sum += int(item[0]) * int(item[1])
+    return sum
+
 
 if __name__ == "__main__":
     main()
